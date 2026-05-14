90e57c958064a7cfe4896f1957952ff9218bacca
y5274210w_iespobleno <y5274210w@iespobleno.org>
2026-05-14 13:57:13 +0200

    working file

---
+++ src/interfaces/MoviesManager.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import model.Movie;
+
+public interface MoviesManager {
+
+    void addMovie(Movie movie);
+    void deleteMovie(Movie movie);
+    void updateMovie(Movie movie);
+
+}
